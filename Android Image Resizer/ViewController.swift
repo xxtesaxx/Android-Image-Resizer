852fecd2fe6d0dfcb06ce81ae77f7dc8ec089bb4
Jan Thielemann <thielemann.jan@gmail.com>
2017-03-16 12:31:45 -0300

    Initial Commit

---
+++ Android Image Resizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Android Image Resizer
+//
+//  Created by Jan Thielemann on 16.03.17.
+//  Copyright © 2017 Jan Thielemann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
